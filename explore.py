2086c052d2cfa2bad4e729fd3e5879c5a94df769
Jack <jackwangzhixiang@gmail.com>
2026-06-29 21:26:06 +1200

    data clean and summary about delays

--- explore.py
+++ explore.py
@@ -1,20 +1,52 @@
 import requests
 import os
 import config
+from collections import defaultdict
 from dotenv import load_dotenv
 
 load_dotenv()
 
 URL = config.TRIP_UPDATES_URL
 SUB_KEY = os.environ["AT_SUB_KEY"]
+MAX_DELAY = 15 * 60
 
-raw_resp= requests.get(URL, headers = {"Ocp-Apim-Subscription-Key": SUB_KEY})
-print("Status Code:", raw_resp.status_code)
-
+raw_resp = requests.get(URL, headers={"Ocp-Apim-Subscription-Key": SUB_KEY})
 data = raw_resp.json()
-response = data["response"]
-print("Keys inside the response: ", response.keys())
-print("Headers: ", response["header"])
+entities = data["response"]["entity"]
 
-entities = response["entity"]
-print("First Entity: ", entities[0])
+print("Number of trips:", len(entities))
+
+rows = []
+for trip in entities:
+    tu = trip["trip_update"]
+    delay = tu.get("delay")
+
+    if delay is None or abs(delay) > MAX_DELAY:  # filter out dirty ones
+        continue
+
+    rows.append(
+        {
+            "route_id": tu["trip"]["route_id"],
+            "delay": delay,
+        }
+    )
+
+
+by_route = defaultdict(list)
+
+for row in rows:
+    by_route[row["route_id"]].append(row["delay"])
+
+summary = []
+
+for route_id, delays in by_route.items():
+    avg_delay = sum(delays) / len(delays)
+    summary.append(
+        {
+            "route_id": route_id,
+            "avg_delay_sec": round(avg_delay, 2),
+            "ongoing_trips": len(delays),
+        }
+    )
+
+summary.sort(key=lambda x: x["avg_delay_sec"], reverse=True)
