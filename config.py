3f758d6b4f3961bd846a053a8f8f588554f13689
Jack <jackwangzhixiang@gmail.com>
2026-06-29 18:42:57 +1200

    Added the config file for url

---
+++ config.py
@@ -0,0 +1,6 @@
+# Real time compat APIs
+COMBINED_FEED_URL = "https://api.at.govt.nz/realtime/legacy/"
+FERRY_POSITION_URL = "https://api.at.govt.nz/realtime/legacy/ferrypositions"
+SERVICE_ALERTS_URL = "https://api.at.govt.nz/realtime/legacy/servicealerts"
+TRIP_UPDATES_URL = "https://api.at.govt.nz/realtime/legacy/tripupdates"
+VEHICLE_POSITIONS_URL = "https://api.at.govt.nz/realtime/legacy/vehiclelocations"
--- explore.py
+++ explore.py
@@ -1,10 +1,11 @@
 import requests
 import os
+import config
 from dotenv import load_dotenv
 
 load_dotenv()
 
-URL = "https://api.at.govt.nz/realtime/legacy/vehiclelocations"
+URL = config.TRIP_UPDATES_URL
 SUB_KEY = os.environ["AT_SUB_KEY"]
 
 raw_resp= requests.get(URL, headers = {"Ocp-Apim-Subscription-Key": SUB_KEY})
@@ -13,3 +14,7 @@ print("Status Code:", raw_resp.status_code)
 data = raw_resp.json()
 response = data["response"]
 print("Keys inside the response: ", response.keys())
+print("Headers: ", response["header"])
+
+entities = response["entity"]
+print("First Entity: ", entities[0])
